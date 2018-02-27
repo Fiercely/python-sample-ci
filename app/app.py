b68703b4a3400597775589f92828e0c307193f6f
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2018-02-27 15:43:27 +0000

    Web Build Trigger and Deploy with running node

--- app/app.py
+++ app/app.py
@@ -8,6 +8,6 @@ def hello():
 
 @app.route("/ci")
 def ci_data():
-    return "I was built by jenkins and my last commit is : {} done by : {}".format(os.getenv("GIT_COMMIT", "FAILED"), os.getenv("GIT_AUTHOR_NAME", "FAILED"))
+    return "I was built by jenkins and my last commit is : {} running on : {}".format(os.getenv("GIT_COMMIT", "FAILED"), os.getenv("NODE_NAME", "FAILED"))
 
 app.run("0.0.0.0", port=5000)
