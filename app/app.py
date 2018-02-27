65c4a5b754140e98bca9b65159cf939fe3ec84c2
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2018-02-27 15:53:31 +0000

    Web revert to basic

--- app/app.py
+++ app/app.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! After triggered build"
+    return "Hello World!"
 
-@app.route("/ci")
-def ci_data():
-    return "I was built by jenkins and my last commit is : {} running on : {}".format(os.getenv("GIT_COMMIT", "FAILED"), os.getenv("NODE_NAME", "FAILED"))
+# @app.route("/ci")
+# def ci_data():
+#     return "I was built by jenkins and my last commit is : {} running on node: {}".format(os.getenv("GIT_COMMIT", "FAILED"), os.getenv("NODE_NAME", "FAILED"))
 
 app.run("0.0.0.0", port=5000)
