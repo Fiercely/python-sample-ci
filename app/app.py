63f4ba969de887296cb9ecf6da2a307b08e3a194
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2018-02-27 15:21:17 +0000

    Web Trigger Build and Deploy

--- app/app.py
+++ app/app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! After trigger build"
+    return "Hello World! After triggered build"
 
 app.run("0.0.0.0", port=5000)
