b6880ab1a24e829e5a52c05976f37c2a73578ac1
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2018-02-27 15:39:42 +0000

    web build trigger and deploy with data

--- app/app.py
+++ app/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World! After triggered build"
+
+@app.route("/ci")
+def ci_data():
+    return "I was built by jenkins and my last commit is : {} done by : {}".format(os.getenv("GIT_COMMIT", "FAILED"), os.getenv("GIT_AUTHOR_NAME", "FAILED"))
 
 app.run("0.0.0.0", port=5000)
