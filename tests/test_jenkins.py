bf3f9a62e9efc81b325fabc6fdb410693345af71
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2018-06-01 19:33:58 +0100

    ADD option for getting other env

--- tests/test_jenkins.py
+++ tests/test_jenkins.py
@@ -3,7 +3,7 @@ import unittest, os
 class Example(unittest.TestCase):
     @classmethod
     def setUp(self):
-        self.branch = os.getenv("sourceBranch")
+        self.branch = os.getenv("sourceBranch", os.getenv("GIT_BRANCH"))
 
     def test_branch(self):
         self.assertEqual(self.branch, "master")
