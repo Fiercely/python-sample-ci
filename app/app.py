c227c763ea2786d3fe461a93e28a3f4c6ff6d361
howcroft <howcroft@gmail.com>
2018-06-02 19:08:46 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
-# @app.route("/ci")
-# def ci_data():
-#     return "I was built by jenkins and my last commit is : {} running on node: {}".format(os.getenv("GIT_COMMIT", "FAILED"), os.getenv("GIT_BRANCH", "FAILED"))
+@app.route("/ci")
+def ci_data():
+    return "I was built by jenkins and my last commit is : {} running on node: {}".format(os.getenv("GIT_COMMIT", "FAILED"), os.getenv("GIT_BRANCH", "FAILED"))
 
 app.run("0.0.0.0", port=5000)
